7c7b1ea3876ef776c5b375431320f94707472b4e
Abhiraman Padala <73153140+abhiraman-rafay@users.noreply.github.com>
2025-04-15 16:17:07 +0530

    Create main.tf

---
+++ baremetal/main.tf
@@ -0,0 +1,9 @@
+variable "gpu_server_type" {
+  description = "GPU server type"
+  type        = string
+}
+
+variable "device_details" {
+  description = "Input as a JSON object"
+  type        = any
+}
